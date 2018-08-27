9c229e82be3ae610919df1050be3ecb81d003c1b
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-27 17:42:28 +0200

    view done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
   get 'items/index'
   get 'items/show'
   devise_for :users
+  get 'user/:id/mon_panier', to: 'cart#show', as: 'mon_panier'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
