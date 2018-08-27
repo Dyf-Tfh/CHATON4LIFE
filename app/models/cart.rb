0d8301c7f970132f2277461e208f600a375c2c68
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-27 19:19:11 +0200

    refactoring db

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,4 @@
 class Cart < ApplicationRecord
-  has_many :items
+  has_many :items, through: :cart_items
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_162609) do
+ActiveRecord::Schema.define(version: 2018_08_27_171547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
@@ -21,11 +30,6 @@ ActiveRecord::Schema.define(version: 2018_08_27_162609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
-  end
-
-  create_table "carts_items", id: false, force: :cascade do |t|
-    t.bigint "item_id", null: false
-    t.bigint "cart_id", null: false
   end
 
   create_table "items", force: :cascade do |t|
