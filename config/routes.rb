75fd4abc095f265054559bdf2e9d6ff3dcf80906
Amélie Foulquier <amelie.foulquier@gmail.com>
2018-08-28 16:04:40 +0200

    new modif header

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'items/show'
   devise_for :users
   get 'user/:id/mon_panier', to: 'cart#show', as: 'mon_panier'
+  get 'user/profile', to: 'user#show', as: 'profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
