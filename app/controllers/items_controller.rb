bdda98cb81292c397766163cfdde2cfbd09b90da
ptalk <pierre.lekhac@sciencespo.fr>
2018-08-28 15:33:56 +0200

    galere sur items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,17 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.all
   end
 
   def show
+    @item.new(params[:item])
   end
+
+  # def new
+  #   @item = Item.new
+  #   @item.proposition_id = params[:proposition_id]
+  # end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p = 0
+5.times do
+  User.create!(email: "nico#{p}@hotmail.fr", password: "loliloli")
 
-1.times do
-  User.create!(email: "n.zhao@hotmail.fr", password: "loliloli")
+  Cart.create!(user_id: "1", total_price: "100")
+
+  Item.create!(title: "savage cat", description:"brown color", price:"200", image_url:"https://www.google.fr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiP5LyUwo_dAhXEKVAKHfoDAWUQjRx6BAgBEAU&url=https%3A%2F%2Fwamiz.com%2Fchats%2Factu%2Fa-26-ans-corduroy-est-le-chat-le-plus-age-du-monde-6724.html&psig=AOvVaw3c8zgj0GDGJ1mIlP70vA9S&ust=1535537786939610" )
+p += 1
 end
-Cart.create!(user_id: "1", total_price: "$100")
