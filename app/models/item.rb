7733e834d4ef2616f9eb5f71fb1e22bb4cf8baae
AyarosF <sorayafructuoso@gmail.com>
2018-08-28 14:16:40 +0200

    order belongs to user

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0 }
   validates :title, :description, :image_url, presence: true
+  has_many :users, through: :carts
   has_and_belongs_to_many :carts
+  has_and_belongs_to_many :orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2018_08_27_162609) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.decimal "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
