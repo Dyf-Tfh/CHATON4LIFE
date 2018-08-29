bb7fce446ff23137d6073a0fb40b87c0931fb71b
davidbelaga <davidbelaga@hotmail.com>
2018-08-30 01:42:44 +0200

    front front here and there

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,4 +7,10 @@ class CartController < ApplicationController
     CartItem.find(params[:id]).items.find(params[:item]).delete
     redirect_to mon_panier_path
   end
+
+  def quantity
+    @b = params[:quantity].to_d
+  end
+
+
 end
