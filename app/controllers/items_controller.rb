b35e4a01bd4c5ed264e338abbfad6ce7fc7d420d
ptalk <pierre.lekhac@sciencespo.fr>
2018-08-29 17:00:30 +0200

    button added to item-show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,20 +3,22 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-    def index
-      @items = Item.all
-    end
-
-    def show
-      @item = Item.find(params[:id])
-    end
-
-    def new
-      @item = Item.new
-    end
-
-    def product_params
-      params.require(:product).permit(:title, :description, :price, :image_url)
-    end
-
+  def show
+    @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image_url)
+  end
+
+  def add
+  	@item = Item.find(params[:id])
+    CartItem.create!(cart_id: current_user.id, item_id: @item.id)
+  	redirect_to mon_panier_path
+  end
+
+end
