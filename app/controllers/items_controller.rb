b7c05b47e98fbe56f0097653fc15e7864cb0b709
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-30 13:10:42 +0200

    oklm routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,7 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
-    pour lier activestorage en local ne sert que durant la config
-    Item.all.each_with index do |item,i|
-     item.small_image.attach(io: File.open('small/cat_') + format("%04d", i) + '.jpg', filename: 'cat_' + format("%04d", i) + '.jpg')
-     item.small_image.attach(io: File.open('large/cat_') + format("%04d", i) + '.png', filename: 'cat_' + format("%04d", i) + '.png')
-    end
+
   end
 
   def show
@@ -24,5 +20,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     CartItem.create!(cart_id: current_user.id, item_id: @item.id)
     redirect_to mon_panier_path
+  end
+
+  def config
+    # pour lier activestorage en local ne sert que durant la config
+    Item.all.each_with index do |item,i|
+      item.small_image.attach(io: File.open('small/cat_') + format("%04d", i) + '.jpg', filename: 'cat_' + format("%04d", i) + '.jpg')
+      item.small_image.attach(io: File.open('large/cat_') + format("%04d", i) + '.png', filename: 'cat_' + format("%04d", i) + '.png')
+     end
   end
 end
