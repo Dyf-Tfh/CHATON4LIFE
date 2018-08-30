6d46dc2352cc542d428426e7d0caaf8bd1bf1fca
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-30 13:36:37 +0200

    anreius$

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,17 +7,12 @@ class CartController < ApplicationController
   end
 
   def delete
-<<<<<<< HEAD
     Cart.find(params[:id]).cart_items.find(item_id: params[:item]).delete
-=======
-    Cart.find(params[:id]).items.find(params[:item]).delete
->>>>>>> development
     redirect_to mon_panier_path
   end
 
   def quantity
     @b = params[:quantity].to_d
   end
-
 
 end
